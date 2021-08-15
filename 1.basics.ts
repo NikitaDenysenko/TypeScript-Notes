35ba373a8ae79625c89b5e4cf48b645568f7c05c
Nikita Denysenko <53399334+NikitaDenysenko@users.noreply.github.com>
2021-08-15 11:35:53 +0300

    initial commit

---
+++ 1.basics.ts
@@ -0,0 +1,24 @@
+//Basic Types
+let userName: string = 'name'
+let hasLoggedIn: boolean = true
+let myNumber: number = 234
+let myRegex: RegExp = /foo/
+
+//Typing Array
+const names: string[] = userName.split(' ')
+const arrNubers: Array<number> = [1, 24, 5]
+
+//Typing Objects
+interface IPerson {
+  name: string
+  age: number
+}
+const person: IPerson = {
+  name: 'Nikc',
+  age: 20
+}
+const ids: Record<number, string> = {
+  10: 'a',
+  20: 'b'
+}
+ids[30] = 'c'
