f3477999f74e982f712fd799a471adfe0fcc26c8
Nikita Denysenko <53399334+NikitaDenysenko@users.noreply.github.com>
2021-08-29 11:34:58 +0300

    added classes

--- 1-Language-Syntax/2.classes.ts
+++ 1-Language-Syntax/2.classes.ts
@@ -1,14 +1,43 @@
 //simple class
-class Car {
-    private name: string;
+abstract class Car {
+  constructor (
+    private readonly name: string,
+    public label: string,
+    protected size: string
+  ) {}
 
-    constructor(name: string) {
-        this.name = name
-    }
+  startEngine (this: Car): void {
+    console.log('Starting engine of ' + this.name)
+  }
 
-    startEngine(this: Car): void {
-        console.log('Starting engine of ' + this.name)
-    }
+  abstract describe (): void
 }
 
-const testCar = new Car('BMW')
+class SportCar extends Car {
+  static releaseYear: number = 1990
+  constructor (name: string) {
+    super(name, 'Sport', 'S')
+  }
+
+  describe () {
+    console.log(`A ${this.label} Car`)
+  }
+
+  get getSportCarLabel () {
+    return this.label
+  }
+
+  set setSportCarLabel (value: string) {
+    this.label = value
+  }
+
+  static createCar (name: string) {
+    return { name }
+  }
+}
+
+const testCar = new Car('BMW', 'Fast', 'M')
+const sportCar = new SportCar('Mustang')
+testCar.label = 'Truck'
+sportCar.setSportCarLabel = 'SuperSport'
+console.log(SportCar.releaseYear)
